4502dadcdcbfb5fa3f7d143be7e54b1218b6af72
Edison Zhang <edisonewtong@163.com>
2021-07-06 01:08:30 +0800

    1. fix some bugs about tool deleteLastEOL.cpp 2.  add options  -f to force delete eol when last character is either \r or \n or \r\n

--- cpp/console/deleteLastEOL/deleteLastEOL.cpp
+++ cpp/console/deleteLastEOL/deleteLastEOL.cpp
@@ -4,17 +4,49 @@
 using namespace std;
 
 
+void printUsage()
+{
+	cout << R"(Usage :
+	$ deleteLastEOL [-f] <filename>
+)" << endl;
+}
 
 int main(int argc, char* argv[], char* env[])
 {
-	if ( argc != 2 ) {
+	if ( argc < 2 ) {
 		cout << "[ERROR] : You must give an existed file name to process " << endl;
+		printUsage();
+		return -1;
+	} else if ( argc >= 4 ) {
+		cout << "[ERROR] : Too many arguments " << endl;
+		printUsage();
 		return -1;
 	}
 
-	ifstream inputFile(argv[1], ios::in | ios::binary);
+	auto forceDeleteOption = false;
+	string filename;
+	if ( argc == 2 ) {
+		filename = argv[1];
+	} else if ( argc == 3 ) {
+		string option(argv[1]);
+		if ( option != "-f" ) {
+			cout << "[ERROR] : Unknow option " << option << endl;
+			printUsage();
+			return -1;
+		} else {
+			forceDeleteOption = true;
+			cout << "[INFO] : Use Force Delete Mode " << endl;
+		}
+		filename = argv[2];
+	}
+
+
+	
+
+
+	ifstream inputFile(filename.c_str() , ios::in | ios::binary);
 	if ( !inputFile ) {
-		cout << "[ERROR] : The given file named \""  << argv[1] << " is not existed. Program exit ! " << endl;
+		cout << "[ERROR] : The given file named \""  << filename << "\" is Not existed !! Program exit ! " << endl;
 		return -1;
 	}
 
@@ -23,7 +55,7 @@ int main(int argc, char* argv[], char* env[])
 	inputFile.seekg(0, ios::beg);
 
 	if ( flength == 0 ) {
-		cout << "[DONE] : File Content is Empty , Do Nothing , Quit! " << endl;
+		cout << "[DONE] : File Content cantain 0 byte , Do Nothing !!! Quit! " << endl;
 		return 0;
 	}
 
@@ -34,7 +66,7 @@ int main(int argc, char* argv[], char* env[])
 	auto bSuccess = false;
 	auto isReallyDeleteEOL = false;
 	string processMsg; 
-	processMsg += ("Can't open file \"" + string(argv[1]) + "\" for writing.");
+	processMsg += ("Can't open file \"" + filename + "\" for writing.");
 	string deleteChars;
 
 	if ( flength == 1ULL ) {
@@ -43,7 +75,7 @@ int main(int argc, char* argv[], char* env[])
 		//
 		if ( content[0] == '\r' || content[0] == '\n' ) {
 			// delete the last only character   from the existed file
-			ofstream outFile( argv[1], ios::out | ios::trunc | ios::binary);
+			ofstream outFile( filename.c_str(), ios::out | ios::trunc | ios::binary);
 			if ( outFile ) {
 				// write nothing into the file , leave an empty file
 				outFile.flush();
@@ -62,7 +94,7 @@ int main(int argc, char* argv[], char* env[])
 		//
 		if ( content[0] == '\r' && content[1] == '\n' ) {
 			// windows version
-			ofstream outFile( argv[1], ios::out | ios::trunc | ios::binary);
+			ofstream outFile( filename.c_str(), ios::out | ios::trunc | ios::binary);
 			if ( outFile ) {
 				// write nothing into the file
 				outFile.close();
@@ -74,14 +106,14 @@ int main(int argc, char* argv[], char* env[])
 		} else if ( content[1] == '\r' || content[1] == '\n' ) {
 			auto needReWrite = false;
 			if ( content[1] == '\r' ) {
-				needReWrite = (content[0] != '\r');
+				needReWrite = forceDeleteOption || (content[0] != '\r');
 			} else {
 				// \n
-				needReWrite = (content[0] != '\n');
+				needReWrite = forceDeleteOption || (content[0] != '\n');
 			}
 
 			if ( needReWrite ) {
-				ofstream outFile( argv[1], ios::out | ios::trunc | ios::binary);
+				ofstream outFile( filename.c_str(), ios::out | ios::trunc | ios::binary);
 				if ( outFile ) {
 					// detele the last byte 
 					outFile.write( content, static_cast<streamsize>(1) );
@@ -106,21 +138,21 @@ int main(int argc, char* argv[], char* env[])
 		unsigned long long lastButTwoIdx = flength - 3;
 		unsigned long long minusChar = 0;
 		if( content[lastButOneIdx] == '\r' && content[lastCharIdx] == '\n' ) {
-			needReWrite = (content[lastButTwoIdx] != '\n');
+			needReWrite = forceDeleteOption || (content[lastButTwoIdx] != '\n');
 			minusChar   = 2;
 			deleteChars = "\\r\\n";
 		} else if ( content[lastCharIdx] == '\r' ) {
-			needReWrite = (content[lastButOneIdx] != '\r');
+			needReWrite = forceDeleteOption || (content[lastButOneIdx] != '\r');
 			minusChar   = 1;
 			deleteChars = "\\r";
 		} else if ( content[lastCharIdx] == '\n' ) {
-			needReWrite = (content[lastButOneIdx] != '\n');
+			needReWrite = forceDeleteOption || (content[lastButOneIdx] != '\n');
 			minusChar   = 1;
 			deleteChars = "\\n";
 		}
 
 		if ( needReWrite ) {
-			ofstream outFile( argv[1], ios::out | ios::trunc | ios::binary);
+			ofstream outFile( filename.c_str(), ios::out | ios::trunc | ios::binary);
 			if ( outFile ) {
 				// detele the last byte 
 				outFile.write( content, static_cast<streamsize>(flength - minusChar) );
@@ -138,7 +170,7 @@ int main(int argc, char* argv[], char* env[])
 	if ( bSuccess ) {
 		if ( isReallyDeleteEOL ) {
 			int deleteCnt = static_cast<int>((deleteChars.size() / 2));
-			cout << "[DONE] : Delete Last " << deleteCnt << (deleteCnt == 1 ? " byte" : " bytes") << endl;
+			cout << "[Over-Write] : Delete Last " << deleteCnt << (deleteCnt == 1 ? " byte" : " bytes") << " => " << deleteChars << " ==========> : \"" << filename << "\"" << endl;
 		} else {
 			cout << "[DONE] : No Need to Process" << endl;
 		}
