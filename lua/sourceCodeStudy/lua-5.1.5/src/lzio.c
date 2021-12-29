b03a360fc7c685dacce18109841bb93936640be9
Edison Zhang <edisonewtong@163.com>
2021-12-29 23:24:32 +0800

    modify  <lzio.c> , Done ( binary compare equal )

--- lua/sourceCodeStudy/lua-5.1.5/src/lzio.c
+++ lua/sourceCodeStudy/lua-5.1.5/src/lzio.c
@@ -22,28 +22,35 @@ int luaZ_fill (ZIO *z) {
   size_t size;
   lua_State *L = z->L;
   const char *buff;
+  /* lua_unlock(L); */
   ((void) 0);
   buff = z->reader(L, z->data, &size);
+  /* lua_lock(L); */
   ((void) 0);
-  if (buff == ((void *)0) || size == 0) {
-    return (-1);
+  if (buff == NULL || size == 0) {
+    /*     (-1) EOZ */
+    return EOZ;
   }
   z->n = size - 1;
   z->p = buff;
-  return ((int)(((unsigned char)((*(z->p++))))));
+  /*      char2int(*(z->p++)) */
+  return (int)( (unsigned char)(*(z->p++)) );
 }
 
 
 int luaZ_lookahead (ZIO *z) {
   if (z->n == 0) {
-    if (luaZ_fill(z) == (-1)) {
-      return (-1);
+    /*                  (-1)  EOZ */
+    if ( luaZ_fill(z) == EOZ ) {
+      /*     (-1) EOZ  */
+      return EOZ;
     } else {
       z->n++; /* luaZ_fill removed first byte; put back it */
       z->p--;
     }
   }
-  return ((int)(((unsigned char)((*z->p)))));
+  /*      char2int(*z->p); */
+  return (int)( (unsigned char)(*z->p) );
 }
 
 
@@ -52,7 +59,7 @@ void luaZ_init (lua_State *L, ZIO *z, lua_Reader reader, void *data) {
   z->reader = reader;
   z->data = data;
   z->n = 0;
-  z->p = ((void *)0);
+  z->p = NULL;
 }
 
 
@@ -60,7 +67,8 @@ void luaZ_init (lua_State *L, ZIO *z, lua_Reader reader, void *data) {
 size_t luaZ_read (ZIO *z, void *b, size_t n) {
   while (n) {
     size_t m;
-    if (luaZ_lookahead(z) == (-1)) {
+    /*                       (-1)  EOZ */
+    if (luaZ_lookahead(z) == EOZ ) {
       return n; /* return number of missing bytes */
     }
     m = (n <= z->n) ? n : z->n; /* min. between n and z->n */
@@ -77,10 +85,18 @@ size_t luaZ_read (ZIO *z, void *b, size_t n) {
 /* ------------------------------------------------------------------------ */
 char *luaZ_openspace (lua_State *L, Mbuffer *buff, size_t n) {
   if (n > buff->buffsize) {
-    if (n < 32) {
-      n = 32;
+    /*              32    LUA_MINBUFFER */
+    if (n < LUA_MINBUFFER) {
+      /*            32   LUA_MINBUFFER */
+      n = LUA_MINBUFFER;
     }
-    ((((buff)->buffer)=((char *)(((((size_t)((n)+1)) <= ((size_t)(~(size_t)0)-2)/(sizeof(char))) ? luaM_realloc_(L, ((buff)->buffer), ((buff)->buffsize)*(sizeof(char)), (n)*(sizeof(char))) : luaM_toobig(L))))), (buff)->buffsize = n);
+    /* luaZ_resizebuffer(L, buff, n); */
+    (  (buff->buffer = (char *)(( (size_t)(n+1) <= MAX_SIZET/sizeof(char) ) 
+                                ? luaM_realloc_(L, buff->buffer, buff->buffsize * sizeof(char), n * sizeof(char) ) 
+                                : luaM_toobig(L) )
+       )
+	   , buff->buffsize = n
+	);
   }
   return buff->buffer;
 }
