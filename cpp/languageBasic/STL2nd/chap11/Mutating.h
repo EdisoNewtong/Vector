ed7353fc228e2e84abd7e2a5324906208539bfc7
Edison Zhang <edisonewtong@163.com>
2022-09-28 00:28:53 +0800

    [dev STL2/Algorithm ] 4. Mutating Algorithm Case ( change the order of the container rather than change the element value

--- cpp/languageBasic/STL2nd/chap11/Mutating.h
+++ cpp/languageBasic/STL2nd/chap11/Mutating.h
@@ -4,6 +4,13 @@ namespace Mutating
 {
     extern void testCaseAll();
 
-}
+	extern void reverseCase();
+	extern void rotateCase();
+	extern void rotate_copyCase();
 
+	extern void permutationCase();
+	extern void shuffleCase();
+
+	extern void partitionCase();
+}
 
