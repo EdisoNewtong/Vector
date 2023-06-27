b822abb631399ffc56586c69d24e5856707cc3a5
Edison Zhang <edisonewtong@163.com>
2023-06-27 23:06:57 +0800

    [dev]  CMake-Study 1. First tutorial for cmake

---
+++ CMake-Study/01-SingleSourceCode/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello C-PlusPlus World " << endl;
+
+    return 0;
+}
+
+
+
---
+++ CMake-Study/Summary.md
@@ -0,0 +1,6 @@
+# This tutorial will guide you get well known about CMake from strach
+
+1. 01. **==Single==** file source code 
+1. 
+
+
