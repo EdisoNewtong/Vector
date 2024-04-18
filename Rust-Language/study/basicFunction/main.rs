141ae7c4ec8c83ab94705bb272d9284081bf742c
Edison Zhang <edisonewtong@163.com>
2024-04-18 22:11:17 +0800

    [Rust Language] 1. Write some useful comment to describe the function defination as detailed as possible

--- Rust-Language/study/basicFunction/main.rs
+++ Rust-Language/study/basicFunction/main.rs
@@ -4,10 +4,12 @@
 //varible a's type is   i32
 //                  varible b's type is   i32
 //                                             return value's type is i32
-fn my_add(a:           i32, b:            i32) ->                     i32
+fn my_add(a:          i32,   // formal parameter  #1 named  'a'
+          b:          i32)   // formal parameter  #2 named  'b'
+->                    i32    // return value type
 {
 	/******************************************************************************************************
-        CAUTION : the return statement must be end without   ';' 
+        CAUTION : the return statement must be end ** without **   ';' 
 	******************************************************************************************************/
     //    this syntax is different from other mainstream programming language 
     a + b
