b02b901779defb48e60aff1c1aa2543c2aa05fd0
Edison Zhang <edisonewtong@163.com>
2024-05-25 16:16:53 +0800

    [dev] Rust Language Enhanced example to show that how to return a value in the middle of a function's body  rathen than at the tail of the body

--- Rust-Language/study/misc/basicFunction/main.rs
+++ Rust-Language/study/misc/basicFunction/main.rs
@@ -16,11 +16,46 @@ fn my_add(a:          i32,   // formal parameter  #1 named  'a'
 }
 
 
+fn my_func_with_conditional_return(a: i32, b: i32) -> i32
+{
+    if a < 0 {
+		// if the return statement is not at the tail of the function's body,
+		//
+		// You *** Must *** use the ** return ** keyword to let the compiler know it's a return statement of the corresponing function
+		// like the following
+
+		// Both 
+		//            return a + b;
+		// And
+		//            return a + b
+		// Are        Valid for the Rust Compiler 
+
+        return a + b;
+        // return a + b   // is an alternative format for the return statement
+
+
+		/*
+
+				42 | |         a + b
+				   | |         ^^^^^ expected `()`, found `i32`
+		*/ 
+		// let _ = a + b;
+    }
+
+    a - b
+}
+
+
 
 fn main()
 {
-	let x = 3;
+	let mut x = 3;
 	let y = 5;
-	println!("{} + {} = {}", x,y, my_add(x,y) );
-}
+	println!("1. {} + {} = {}", x,y, my_add(x,y) );
 
+	x = -3;
+	println!("2. {} + {} = {}", x,y, my_func_with_conditional_return(x,y) );
+
+	x = 15;
+	println!("3. {} - {} = {}", x,y, my_func_with_conditional_return(x,y) );
+}
