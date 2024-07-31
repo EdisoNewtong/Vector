5a38223fd1ecf349c9340b1ef48556f778d21ccd
Edison Zhang <edisonewtong@163.com>
2024-07-31 17:03:00 +0800

    [dev] cpp -> dataStruct-algorithm  Graph DFS ( Depth First Search )  &&   BFS ( Breath First Search )   example graph  and an illustration

--- cpp/dataStruct-algorithm/graph/main.cpp
+++ cpp/dataStruct-algorithm/graph/main.cpp
@@ -5,7 +5,7 @@
 #include <set>
 using namespace std;
 
-#define NODE_CNT        5
+#define NODE_CNT                13 
 #define NODE_CNT_2D_TABLE_1  7
 #define NODE_CNT_2D_TABLE_2  6
 #define INF_DIS            -1
@@ -13,7 +13,7 @@ using namespace std;
 typedef struct ArcNode {
 	int adjNodeTag;
 	struct ArcNode* nextArc = nullptr;
-	int weight;
+	int weight = 1;
 } ArcNode;
 
 typedef struct VNode {
@@ -181,6 +181,9 @@ void Dijkstra1( const Graph2D_1& g, int baseVertexTag, minPath minpathAry[NODE_C
         g_unProcessVertex.erase( g_unProcessVertex.find(pickedminTag) );
     } while ( !g_unProcessVertex.empty() );
 
+	cout << "--------------------------------------------------------------------------------" << endl;
+	cout << "Dijkstra-1 --> find the minpath from basePoint [" << baseVertexTag << "] --> Others : " << endl;
+	cout << "--------------------------------------------------------------------------------" << endl;
     // output 
     for( auto i = 0; i < NODE_CNT_2D_TABLE_1; ++i ) {
         cout << "[" << baseVertexTag  << "] -> [" << i << "]   Dis = " << minpathAry[i].dis << ", minPath = ";
@@ -273,6 +276,10 @@ void Dijkstra2( const Graph2D_2& g, int baseVertexTag, minPath minpathAry[NODE_C
         g_unProcessVertex.erase( g_unProcessVertex.find(pickedminTag) );
     } while ( !g_unProcessVertex.empty() );
 
+
+	cout << "--------------------------------------------------------------------------------" << endl;
+	cout << "Dijkstra-2 --> find the minpath from basePoint [" << baseVertexTag << "] --> Others : " << endl;
+	cout << "--------------------------------------------------------------------------------" << endl;
     //
     // output 
     //
@@ -424,23 +431,233 @@ void test_Dijkstra_2()
 }
 
 
+AdjGraph* createAdjGraph(vector<ArcNode*>& collectedArcList)
+{
+	AdjGraph* pGraph = new AdjGraph;
+	pGraph->n = 13;
+	pGraph->e = 15;
+	for( auto i = 0; i < pGraph->n; ++i ) {
+		pGraph->adjlist[i].tag = i;
+	}
+
+	// 0
+	auto arcNodeFor0_1 = new ArcNode;
+	arcNodeFor0_1->adjNodeTag = 7;
+	auto arcNodeFor0_2 = new ArcNode;
+	arcNodeFor0_2->adjNodeTag = 9;
+	auto arcNodeFor0_3 = new ArcNode;
+	arcNodeFor0_3->adjNodeTag = 11;
+
+	arcNodeFor0_1->nextArc = arcNodeFor0_2;
+	arcNodeFor0_2->nextArc = arcNodeFor0_3;
+	pGraph->adjlist[0].firstArc = arcNodeFor0_1;
+
+	collectedArcList.push_back( arcNodeFor0_1 );
+	collectedArcList.push_back( arcNodeFor0_2 );
+	collectedArcList.push_back( arcNodeFor0_3 );
+
+
+	// 1
+	auto arcNodeFor1_1 = new ArcNode;
+	arcNodeFor1_1->adjNodeTag = 8;
+	auto arcNodeFor1_2 = new ArcNode;
+	arcNodeFor1_2->adjNodeTag = 10;
+
+	arcNodeFor1_1->nextArc = arcNodeFor1_2;
+	pGraph->adjlist[1].firstArc = arcNodeFor1_1;
+
+	collectedArcList.push_back( arcNodeFor1_1 );
+	collectedArcList.push_back( arcNodeFor1_2 );
+
+	// 2
+	auto arcNodeFor2_1 = new ArcNode;
+	arcNodeFor2_1->adjNodeTag = 3;
+	auto arcNodeFor2_2 = new ArcNode;
+	arcNodeFor2_2->adjNodeTag = 12;
+
+	arcNodeFor2_1->nextArc = arcNodeFor2_2;
+	pGraph->adjlist[2].firstArc = arcNodeFor2_1;
+
+	collectedArcList.push_back( arcNodeFor2_1 );
+	collectedArcList.push_back( arcNodeFor2_2 );
+
+	// 3
+	auto arcNodeFor3_1 = new ArcNode;
+	arcNodeFor3_1->adjNodeTag = 2;
+	auto arcNodeFor3_2 = new ArcNode;
+	arcNodeFor3_2->adjNodeTag = 4;
+	auto arcNodeFor3_3 = new ArcNode;
+	arcNodeFor3_3->adjNodeTag = 7;
+
+	arcNodeFor3_1->nextArc = arcNodeFor3_2;
+	arcNodeFor3_2->nextArc = arcNodeFor3_3;
+	pGraph->adjlist[3].firstArc = arcNodeFor3_1;
+
+	collectedArcList.push_back( arcNodeFor3_1 );
+	collectedArcList.push_back( arcNodeFor3_2 );
+	collectedArcList.push_back( arcNodeFor3_3 );
+
+	// 4
+	auto arcNodeFor4_1 = new ArcNode;
+	arcNodeFor4_1->adjNodeTag = 3;
+
+	pGraph->adjlist[4].firstArc = arcNodeFor4_1;
+
+	collectedArcList.push_back( arcNodeFor4_1 );
+
+	// 5
+	auto arcNodeFor5_1 = new ArcNode;
+	arcNodeFor5_1->adjNodeTag = 6;
+
+	pGraph->adjlist[5].firstArc = arcNodeFor5_1;
+
+	collectedArcList.push_back( arcNodeFor5_1 );
+
+	// 6
+	auto arcNodeFor6_1 = new ArcNode;
+	arcNodeFor6_1->adjNodeTag = 5;
+	auto arcNodeFor6_2 = new ArcNode;
+	arcNodeFor6_2->adjNodeTag = 7;
+
+	pGraph->adjlist[6].firstArc = arcNodeFor6_1;
+
+	collectedArcList.push_back( arcNodeFor6_1 );
+
+	// 7
+	auto arcNodeFor7_1 = new ArcNode;
+	arcNodeFor7_1->adjNodeTag = 0;
+	auto arcNodeFor7_2 = new ArcNode;
+	arcNodeFor7_2->adjNodeTag = 3;
+	auto arcNodeFor7_3 = new ArcNode;
+	arcNodeFor7_3->adjNodeTag = 6;
+	auto arcNodeFor7_4 = new ArcNode;
+	arcNodeFor7_4->adjNodeTag = 11;
+
+	arcNodeFor7_1->nextArc = arcNodeFor7_2;
+	arcNodeFor7_2->nextArc = arcNodeFor7_3;
+	arcNodeFor7_3->nextArc = arcNodeFor7_4;
+
+	pGraph->adjlist[7].firstArc = arcNodeFor7_1;
+
+	collectedArcList.push_back( arcNodeFor7_1 );
+	collectedArcList.push_back( arcNodeFor7_2 );
+	collectedArcList.push_back( arcNodeFor7_3 );
+	collectedArcList.push_back( arcNodeFor7_4 );
+
+	// 8
+	auto arcNodeFor8_1 = new ArcNode;
+	arcNodeFor8_1->adjNodeTag = 1;
+	auto arcNodeFor8_2 = new ArcNode;
+	arcNodeFor8_2->adjNodeTag = 9;
+	auto arcNodeFor8_3 = new ArcNode;
+	arcNodeFor8_3->adjNodeTag = 12;
+
+	arcNodeFor8_1->nextArc = arcNodeFor8_2;
+	arcNodeFor8_2->nextArc = arcNodeFor8_3;
+
+	pGraph->adjlist[8].firstArc = arcNodeFor8_1;
+
+	collectedArcList.push_back( arcNodeFor8_1 );
+	collectedArcList.push_back( arcNodeFor8_2 );
+	collectedArcList.push_back( arcNodeFor8_3 );
+
+	// 9
+	auto arcNodeFor9_1 = new ArcNode;
+	arcNodeFor9_1->adjNodeTag = 0;
+	auto arcNodeFor9_2 = new ArcNode;
+	arcNodeFor9_2->adjNodeTag = 8;
+	auto arcNodeFor9_3 = new ArcNode;
+	arcNodeFor9_3->adjNodeTag = 10;
+
+	arcNodeFor9_1->nextArc = arcNodeFor9_2;
+	arcNodeFor9_2->nextArc = arcNodeFor9_3;
+
+	pGraph->adjlist[9].firstArc = arcNodeFor9_1;
+
+	collectedArcList.push_back( arcNodeFor9_1 );
+	collectedArcList.push_back( arcNodeFor9_2 );
+	collectedArcList.push_back( arcNodeFor9_3 );
+
+	// 10 
+	auto arcNodeFor10_1 = new ArcNode;
+	arcNodeFor10_1->adjNodeTag = 1;
+	auto arcNodeFor10_2 = new ArcNode;
+	arcNodeFor10_2->adjNodeTag = 9;
+
+	arcNodeFor10_1->nextArc = arcNodeFor10_2;
+
+	pGraph->adjlist[10].firstArc = arcNodeFor10_1;
+
+	collectedArcList.push_back( arcNodeFor10_1 );
+	collectedArcList.push_back( arcNodeFor10_2 );
+
+	// 11 
+	auto arcNodeFor11_1 = new ArcNode;
+	arcNodeFor11_1->adjNodeTag = 0;
+	auto arcNodeFor11_2 = new ArcNode;
+	arcNodeFor11_2->adjNodeTag = 7;
+
+	arcNodeFor11_1->nextArc = arcNodeFor11_2;
+
+	pGraph->adjlist[11].firstArc = arcNodeFor11_1;
+
+	collectedArcList.push_back( arcNodeFor11_1 );
+	collectedArcList.push_back( arcNodeFor11_2 );
+
+	// 12 
+	auto arcNodeFor12_1 = new ArcNode;
+	arcNodeFor12_1->adjNodeTag = 2;
+	auto arcNodeFor12_2 = new ArcNode;
+	arcNodeFor12_2->adjNodeTag = 8;
+
+	arcNodeFor12_1->nextArc = arcNodeFor12_2;
+
+	pGraph->adjlist[12].firstArc = arcNodeFor12_1;
+
+	collectedArcList.push_back( arcNodeFor12_1 );
+	collectedArcList.push_back( arcNodeFor12_2 );
+
+
+
+	return pGraph;
+}
+
+void test_DFS(AdjGraph* g)
+{
+	cout << "---------- DFS Result ----------" << endl;
+	for( auto i = 0; i < NODE_CNT; ++i ) {
+		visited_flag[i] = 0;
+	}
+
+	DFS(*g, 0);
+}
+
+void test_BFS(AdjGraph* g)
+{
+	cout << "---------- BFS Result ----------" << endl;
+	for( auto i = 0; i < NODE_CNT; ++i ) {
+		visited_flag[i] = 0;
+	}
+
+	BFS(*g, 0);
+}
+
+
 
 int main(int argc, char* argv[], char* env[])
 {
 
-    /*
-	AdjGraph g;
+	vector<ArcNode*> arcList;
+	AdjGraph* g = createAdjGraph(arcList);
 
-	for( auto i = 0; i < NODE_CNT; ++i ) {
-		visited_flag[i] = 0;
+	test_DFS(g);
+	cout << endl << "--------------------------------------------------" << endl << endl;
+	test_BFS(g);
+	cout << endl << endl;
+	delete g; g = nullptr;
+	for( auto it = arcList.begin(); it != arcList.end(); ++it ) {
+		delete *it; *it = nullptr;
 	}
-	DFS(g, 0);
-
-	for( auto i = 0; i < NODE_CNT; ++i ) {
-		visited_flag[i] = 0;
-	}
-	BFS(g, 0);
-    */
 
 
     test_Dijkstra_1();
@@ -451,6 +668,5 @@ int main(int argc, char* argv[], char* env[])
 
 	return 0;
 }
-
 
 
