9fe971e9a1594cc065d0c513eb5c0aaee1289888
EdisoNewton <edisonewtong@163.com>
2025-01-04 22:13:06 +0800

    [dev] Rust Language enum   Sample Code

--- Rust-Language/study/beginner/Chap06-enum/main.rs
+++ Rust-Language/study/beginner/Chap06-enum/main.rs
@@ -142,9 +142,9 @@ enum Message {
 }
 
 impl Message {
-	// Apply some certain member functions
-	fn response_message(&self) {
-	}
+    // Apply some certain member functions
+    fn response_message(&self) {
+    }
 }
 
 
@@ -153,13 +153,13 @@ fn case2_test_special_form_of_enum()
     println!();
     println!("----- #2 -----   Special kind of enum : Example case -----");
 
-	//                Quit the Program ?
+    //                Quit the Program ?
     let m1: Message = Message::Quit;
-	//                                move to the destination position (12, 24)
+    //                                move to the destination position (12, 24)
     let m2: Message = Message::Move  { x:12, y:24 };
-	//                                Write some text onto the screen
+    //                                Write some text onto the screen
     let m3: Message = Message::Write ( String::from("Hello World ") );
-	//                                change the text's color 
+    //                                change the text's color 
     let m4: Message = Message::ChangeColor(255, 100, 30); // RGB color
     
     // let xval: i32 = (Option::<Message::Move>(m2)).x;
@@ -172,7 +172,7 @@ fn case2_test_special_form_of_enum()
     println!("m3 = {:?}", m3);
     println!("m4 = {:?}", m4);
 
-	m2.response_message();
+    m2.response_message();
 }
 
 /*
@@ -200,28 +200,218 @@ String hello_world = str_hello + str_world; // Run-time Error
 Option<T>
 enum Option<T> {
     Some(T),
-	None,
+    None,
 }
 
 
 */
 
 #[warn(unused_variables)]
-fn test_option_case() 
+fn test_option_case1() 
 {
     println!();
-    println!("----- Option<T> enum Sample code -----");
-	let _some_number = Some(5);              // Option<i32>
-	let _some_string = Some("A String");     // Option<&str>
+    println!("----- Option<T> enum Sample Code 1 -----");
+    let some_number = Some(5);              // Option<i32>
+    let some_string = Some("A String");     // Option<&str>
 
     // Core Core Core : Type   'Option<i32>' is a must  
-	//    Otherwise the Rust compiler cannot deduce what is the type specified in Option<T>   ( Because 'None' is a possibility  enumeration of Option<T> )  
-	let _absent_number: Option<i32> = None;  
-	
+    //    Otherwise the Rust compiler cannot deduce what is the type specified in Option<T>   ( Because 'None' is a possibility  enumeration of Option<T> )  
+    let absent_number: Option<i32> = None;  
+    println!("some_number = {:?}", some_number);
+    println!("some_string = {:?}", some_string);
+    println!("absent_number = {:?}", absent_number);
+    
 }
 
 
-fn main() {
+fn test_option_case2() 
+{
+    println!();
+    println!("----- Option<T> enum Sample Code 2-----");
+    let a: i32 = 5;
+    let b  =  Some(6);
+    let c: i32 = a + b.unwrap();
+    println!("c = {}", c);
+}
+
+/***************************************************
+Keyword 'match'     pairing
+强大的控制流运算符 - match
+- 允许一个值与一系列模式匹配， 并执行匹配的模式对应的代码
+- 模式可以是 字面值、 变量名、 通配符 , ...
+- match 匹配必须穷举所有的可能 ( or use      '_'  通配符 ) 否则编译器报错
+
+****************************************************/
+
+enum USACoin {
+    Penny,    // 1  便士
+    Nickel,   // 5  美分
+    Dime,     // 10 美分
+    Quarter,  // 25 美分
+}
+
+
+fn value_in_cents(coin: USACoin) -> u8 {
+    match coin {
+        /********************************************************************
+           if the code is more than 1 line , Please use  { 
+                 ...
+           }
+
+
+           e.g. 
+
+           USACoin::Penny => {   
+              println!("match Penny");
+              1 
+           }
+        *********************************************************************/
+        //
+        // Core Core Core : Rust will follow the order by your code 
+        //
+        USACoin::Penny     =>  1,
+        USACoin::Nickel    =>  5,
+        USACoin::Dime      =>  10,
+        USACoin::Quarter   =>  25
+    }
+}
+
+
+
+
+
+fn test_match_expression()
+{
+    println!();
+    println!("----- test KeyWord 'match' -----");
+    let num1: u8 = value_in_cents(USACoin::Penny);
+    let num2: u8 = value_in_cents(USACoin::Nickel);
+    let num3: u8 = value_in_cents(USACoin::Dime);
+    let num4: u8 = value_in_cents(USACoin::Quarter);
+    println!("value of Penny   = {}", num1);
+    println!("value of Nickel  = {}", num2);
+    println!("value of Dime    = {}", num3);
+    println!("value of Quarter = {}", num4);
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+
+enum USACoin2 {
+    Penny,    // 1  便士
+    Nickel,   // 5  美分
+    Dime,     // 10 美分
+    Quarter(UsState),  // 25 美分
+}
+
+
+fn value_in_cents2(coin: USACoin2) -> u8 {
+    match coin {
+        //
+        // Core Core Core : Rust will follow the order by your code 
+        //
+        USACoin2::Penny     =>  {
+             println!("Penny");
+             1
+        }, // <== Notes , Here ','    comma is an optional delimiter  : Either { ... }    or  { ... },    is valid
+        USACoin2::Nickel    =>  5,
+        USACoin2::Dime      =>  10,
+        USACoin2::Quarter(state)   =>  {
+             println!("State quarter from {:?}", state); 
+             25
+        }
+    }
+}
+
+fn test_match_expression2()
+{
+    println!();
+    println!("----- test KeyWord 'match' with binding data -----");
+    let c: USACoin2 = USACoin2::Quarter(UsState::Alaska);
+    println!("{}", value_in_cents2(c));
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32>
+{
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
+}
+
+
+fn test_match_expression3()
+{
+    println!();
+    println!("----- test KeyWord 'match' pairing on Option<T> -----");
+    let five: Option<i32> = Some(5);
+    let six: Option<i32> = plus_one(five);
+    let none: Option<i32> = plus_one(None);
+    // Core Core Core : use method  unwrap() on enumObject
+    println!("six = {}", six.unwrap() );
+    // use is_none() on a certain enumObject
+    if none.is_none() {
+        println!("none = Option<i32>::None" );
+    } else {
+        println!("none = {}", none.unwrap() );
+    }
+}
+
+
+
+fn test_match_expression4()
+{
+    println!();
+    println!("----- test KeyWord 'match' use '_' wildcard to process some condition not be written in code -----");
+    let val: u8 = 123u8;
+    match val {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => {
+            println!("default match : val = {}",val);
+            ()  // return empty tuple
+        },
+    }
+}
+
+
+/***********************************************************************************
+
+if let
+- is a simple version of 'match' block
+- 处理只关心一种匹配而忽略其它匹配的情况
+- 更少的代码，更少的缩进，更少的模板代码
+- 放弃了穷举的可能
+- 可以把 if let 看作是 match 的 语法糖
+
+
+***********************************************************************************/
+
+fn test_if_let()
+{
+    println!();
+    println!("----- test  <if let> expression -----");
+    let val1: Option<i32> = Some(3i32);
+    let val2: Option<i32> = Some(-123i32);
+    if let Some(3) = val1 {
+        println!("val#1 is  three");
+    }
+
+    if let Some(3) = val2 { 
+        println!("val#2 is  three");
+    } else {
+        println!("in else : val2 = {}", val2.unwrap() );
+	}
+}
+
+fn main() 
+{
     let ipv4: IpAddrType = IpAddrType::V4;
     let ipv6: IpAddrType = IpAddrType::V6;
  
@@ -235,6 +425,19 @@ fn main() {
 
     case2_test_special_form_of_enum();
 
-	test_option_case();
+    test_option_case1();
+
+    test_option_case2();
+
+    test_match_expression();
+
+    test_match_expression2();
+
+    test_match_expression3();
+    
+    test_match_expression4();
+
+    test_if_let();
 
 }
+
