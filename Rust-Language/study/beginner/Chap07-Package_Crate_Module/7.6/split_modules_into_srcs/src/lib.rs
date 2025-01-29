b00a90de8c66d4f526fbc0ec00b00fc0f559b149
EdisoNewton <edisonewtong@163.com>
2025-01-29 09:15:43 +0800

    [dev] Rust Language Chap07   7.6   Add some notes to make it easy to understand

--- Rust-Language/study/beginner/Chap07-Package_Crate_Module/7.6/split_modules_into_srcs/src/lib.rs
+++ Rust-Language/study/beginner/Chap07-Package_Crate_Module/7.6/split_modules_into_srcs/src/lib.rs
@@ -1,10 +1,32 @@
 // import the source code file whose name is  front_of_house.rs
 mod front_of_house;
 
+/*********************************************************
+ *
+//////////////////////////////////////////////////
+//
+// Original source code 
+//
+//////////////////////////////////////////////////
+ 
+mod front_of_house 
+{
+    pub mod hosting 
+    {
+        pub fn add_to_waitlist() 
+        {
+	        println!("@ front_of_house/hosting.rs");
+        }
+    }
+}
+
+*********************************************************/
 pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant()
 {
     hosting::add_to_waitlist();
 }
+
+
 
