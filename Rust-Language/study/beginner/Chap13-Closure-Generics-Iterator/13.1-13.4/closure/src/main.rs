fea13a305a862d855891b67fb8e81ac34ab6409e
EdisoNewton <edisonewtong@163.com>
2025-12-17 16:19:05 +0800

    [dev] Rust Language 1. upload Chap 13.3 and its notes

--- Rust-Language/study/beginner/Chap13-Closure-Generics-Iterator/13.1-13.4/closure/src/main.rs
+++ Rust-Language/study/beginner/Chap13-Closure-Generics-Iterator/13.1-13.4/closure/src/main.rs
@@ -15,7 +15,49 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+    where T: Fn(u32) -> u32 
+{
+    calculation: T,
+    // Why use Option Type wrap
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //
+    //    Because the closure can be only defined without any call
+    // Before the closure execute , the value is Option<u32>::None
+    //
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    value: Option<u32>,
+}
 
+impl<T> Cacher<T> 
+    where T: Fn(u32) -> u32, 
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    //
+    // Why use mut keyword : because the  member data named 'value' will be changed during run-time 
+    //
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            // if self.value is None
+            // it means the closure haven't been executed yet!
+            //   call the closure only once
+            None    => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+
+
+}
 
 
 fn main() {
@@ -27,15 +69,17 @@ fn main() {
 
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    // must add a new keyword  mut before the varible name "expensive_closure"
+    // because the closure will change the internal data's value of Object Cacher<u32>
+    let mut expensive_closure = Cacher::new( |num| {
         println!("calculating slowly...");
         thread::sleep( Duration::from_secs(2) ); // sleep for 2 seconds
         num
-    };
+    } );
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure(intensity) );
-        println!( "Next, do {} situps!", expensive_closure(intensity) );
+        println!("Today, do {} pushups!", expensive_closure.value(intensity) );
+        println!( "Next, do {} situps!", expensive_closure.value(intensity) );
     } else {
         if random_number == 3 {
             // ?????????????????????????????????????????????????????????????????????????????
@@ -46,15 +90,31 @@ fn generate_workout(intensity: u32, random_number: u32) {
             // ?????????????????????????????????????????????????????????????????????????????
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!( "Today, run for {} minutes!", expensive_closure(intensity) );
+            println!( "Today, run for {} minutes!", expensive_closure.value(intensity) );
         }
     }
 
 }
 
 
+#[cfg(test)]
+mod tests {
 
+    #[test]
+    fn call_with_different_valus() {
+        let mut c = super::Cacher::new(|a| a);
+        let v1 = c.value(1);
 
+        // The value of c.value(xxx) will not changed forever ( always return 1 )
+        // because it has been locked after the 1st call 
+        // always keep   return 1 for the upcomming calling
+        let v2 = c.value(2);
+
+        // the statement will got panic when execute the command :    
+        //     $ cargo test
+        assert_eq!( v2, 2 ); 
+    }
+}
 
 
 
